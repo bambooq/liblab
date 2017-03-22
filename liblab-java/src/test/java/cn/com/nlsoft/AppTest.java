ff8456b01bfddd8baf195304bad92461974662e3
zhengqi <bamboo625>
2017-03-22 17:03:01 +0800

    mybatis

--- liblab-java/src/test/java/cn/com/nlsoft/AppTest.java
+++ liblab-java/src/test/java/cn/com/nlsoft/AppTest.java
@@ -1,9 +1,22 @@
 package cn.com.nlsoft;
 
 
+import org.junit.Test;
+
 /**
  * Unit test for simple App.
  */
 public class AppTest {
+
+
+    @Test
+    public void test() {
+        StackTraceElement[] callStack = Thread.currentThread().getStackTrace();
+        for (StackTraceElement s : callStack) {
+            System.out.println("s.getClassName() -> " + s.getClassName());
+            System.out.println("s.getMethodName() -> " + s.getMethodName());
+            System.out.println("s.getLineNumber() -> " + s.getLineNumber());
+        }
+    }
 
 }
